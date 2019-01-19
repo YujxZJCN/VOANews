6c4ee1f028acb6603ebb9b100a4a5e3f513101f8
俞佳兴 <albertyu@ys-macbook-pro.local>
2019-01-19 13:23:22 +0800

    Initial Commit

---
+++ VOANews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VOANews
+//
+//  Created by 俞佳兴 on 2019/1/19.
+//  Copyright © 2019 Albert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
